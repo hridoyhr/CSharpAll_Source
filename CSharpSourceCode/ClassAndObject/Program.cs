0b1a578d7aeeb2e900ad075048ee8f5e99a079c1
hridoyhr <hridoyswe205@gmail.com>
2021-04-26 23:31:05 +0600

    Class and Object

---
+++ CSharpSourceCode/ClassAndObject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point(0, 0);
+            var p2 = new Point(2,3);
+
+        }
+    }
+}
