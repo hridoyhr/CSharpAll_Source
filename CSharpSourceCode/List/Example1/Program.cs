8f703c0c26e1672014f9bd7801d2d64c45861866
hridoyhr <hridoyswe205@gmail.com>
2021-04-28 12:23:11 +0600

    Search and sort list

--- CSharpSourceCode/List/Example1/Program.cs
+++ CSharpSourceCode/List/Example1/Program.cs
@@ -11,13 +11,33 @@ namespace Example1
             names.Remove("rony");
             names.Add("Rofiq");
             names.Add("Nabil");
-            foreach (var name in names)
+            //foreach (var name in names)
+            //{
+            //    Console.WriteLine($"Hello {name.ToUpper()}");
+            //}
+            //Console.WriteLine($"My name is {names[0]}");
+            //Console.WriteLine($"I ve added {names[2]} and {names[3]} to list");
+            //Console.WriteLine($"Total List {names.Count}");
+
+            var index = names.IndexOf("Jakir");
+            if(index == -1)
             {
-                Console.WriteLine($"Hello {name.ToUpper()}");
+                Console.WriteLine($"When an item is not found, IndexOf return {index}");
             }
-            Console.WriteLine($"My name is {names[0]}");
-            Console.WriteLine($"I ve added {names[2]} and {names[3]} to list");
-            Console.WriteLine($"Total List {names.Count}");
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index{index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if(index == -1)
+            {
+                Console.WriteLine($"When  an item is not found,Index of return {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name{names[index]} is at idex {index}");
+            }
         }
     }
 }
