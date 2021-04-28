814a31c5e722c42b5bbf907cb445f4a83ed2c8d0
hridoyhr <hridoyswe205@gmail.com>
2021-04-28 12:34:14 +0600

    sort list

--- CSharpSourceCode/List/SearchAndSortList/Program.cs
+++ CSharpSourceCode/List/SearchAndSortList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SearchAndSortList
 {
@@ -6,7 +7,28 @@ namespace SearchAndSortList
     {
         static void Main(string[] args)
         {
-            
+            var names = new List<string> { "Hridoy", "ZOZo", "Akbar", "ebraHIm" };
+            names.Add("rezuawn");
+
+            var index = names.IndexOf("Hasib");
+            if(index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
         }
     }
 }
