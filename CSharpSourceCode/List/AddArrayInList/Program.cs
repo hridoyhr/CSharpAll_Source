f5127f309620864c0bd18bb5cf31230a01ce557f
hridoyhr <hridoyswe205@gmail.com>
2021-04-28 13:55:03 +0600

    List method

---
+++ CSharpSourceCode/List/AddArrayInList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddArrayInList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = new string[3] { "Dhaka", "Brasal", "Commilla" };
+
+            var popularCities = new List<string>();
+
+            popularCities.AddRange(cities);
+
+            var favouriteCities = new List<string>();
+            favouriteCities.AddRange(popularCities);
+            foreach(var print in favouriteCities)
+            {
+                Console.WriteLine(print);
+            }
+        }
+    }
+}
