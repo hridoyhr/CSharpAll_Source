9b5c5584902e1ed415d4136de5f8e8cdde10ec23
hridoyhr <hridoyswe205@gmail.com>
2021-05-03 19:52:12 +0600

    Encapsulation

---
+++ CSharpAllTopic/MethodEx-1/NumberManipulator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodEx_1
+{
+    public class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+                result = num2;
+
+            return result;
+        }
+    }
+}
