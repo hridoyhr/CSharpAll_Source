6390a499958bd197bc964389b0293bf543be8936
hridoyhr <hridoyswe205@gmail.com>
2021-05-06 00:42:39 +0600

    Enum

---
+++ CSharpAllTopic/OperatorEx-1/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorEx_1
+{
+    public class Box
+    {
+        private double Length;
+        private double Width;
+        private double Height;
+
+        public double getVolume()
+        {
+            return Length * Width * Height;
+        }
+
+        public void SetLenght(double length)
+        {
+            Length = length;
+        }
+        public void setWidth(double width)
+        {
+            Width = Length;
+        }
+        public void setHeight(double height)
+        {
+            Height = height;
+        }
+    }
+}
