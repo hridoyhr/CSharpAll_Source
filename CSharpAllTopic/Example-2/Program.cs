5709d8fb25693c6ea9004826c349d1745082a6ed
hridoyhr <hridoyswe205@gmail.com>
2021-05-06 14:51:41 +0600

    Method, Array

---
+++ CSharpAllTopic/Example-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Example_2
+{
+    class Program
+    {
+        private static void Showmatch(string text, string expr)
+        {
+            Console.WriteLine("The Expression", expr);
+            MatchCollection mc = Regex.Matches(text, expr);
+
+            foreach(Match m in mc)
+            {
+                Console.WriteLine(m);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string str = "A thousand Splendid Suns";
+
+            Console.WriteLine("Mactching word 'S' ");
+            Showmatch(str, @"\bs\S*");
+        }
+    }
+}
